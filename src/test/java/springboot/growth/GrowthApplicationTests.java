ad3c60c829ec92d4c886efa1bef47995713c21ed
KimJoongHyeok <moon960427@gmail.com>
2023-09-04 22:36:09 +0900

    Initial commit

---
+++ src/test/java/springboot/growth/GrowthApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.growth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
